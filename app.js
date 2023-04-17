3714f6f6e54261c4cdaa50a6265dacd3b4b55bf5
zai-i <zainab@wonderbly.com>
2023-04-17 13:25:56 +0100

    Vercel config

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ var corsOptions = {
 }
 
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv-safe').config();
+  require('dotenv-safe').config()
   app.use(cors())
 }
 app.get('/', async (req, res) => {
@@ -27,14 +27,14 @@ app.get('/api', cors(corsOptions), async (req, res) => {
       'X-RapidAPI-Host': process.env.RAPID_API_HOST,
     },
     body: `{"model":"gpt-3.5-turbo","messages":[{"role":"user","content": "Generate one haiku from the following keywords: ${req.query.keywords}."}]}`,
-  };
+  }
   try {
     const response = await fetch(process.env.RAPID_API_URL, options)
-    const json = await response.json();
+    const json = await response.json()
     res.send(json)
   }
   catch (error) {
-    console.error(error);
+    console.error(error)
     res.send(error)
   }
 })
@@ -42,3 +42,5 @@ app.get('/api', cors(corsOptions), async (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+module.exports = app
