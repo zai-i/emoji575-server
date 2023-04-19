76d43ba3d01d254ec21202670ec80e49cc415091
zai-i <zainab@wonderbly.com>
2023-04-19 20:59:33 +0100

    Add cors info

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const port = 3000
 const cors = require('cors')
 
 var corsOptions = {
-  origin: ['https://emoji575.zaiz.ai', 'http://127.0.0.1:5173'],
+  origin: ['https://emoji575.zaiz.ai', 'http://127.0.0.1:5173', 'http://localhost:5173'],
   optionsSuccessStatus: 200 
 }
 
