e859783b32ec3b5c2db5970892cb32fc24851724
zai-i <zainab@wonderbly.com>
2023-05-17 17:52:07 +0100

    Test

--- api/index.js
+++ api/index.js
@@ -93,18 +93,10 @@ app.get('/api', cors(corsOptions), async (req, res) => {
       res.send(haiku)
     }
     else {
-      res.status(200).send('')
-      fetch(req.query.response_url,
-        {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: `{
-          "text": "Generating a valid haiku...",
-          "response_type": "in_channel"
-        }`
-      })
+      res.status(200).send(`{
+        "text": "Generating a valid haiku...",
+        "response_type": "in_channel"
+      }`)
       fetch(req.query.response_url,
         {
         method: 'POST',
