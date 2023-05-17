5ae683118df1ce8ba4b031743f69009a3402db4d
zai-i <zainab@wonderbly.com>
2023-05-17 22:04:16 +0100

    Test

--- api/index.js
+++ api/index.js
@@ -93,18 +93,6 @@ app.get('/api', cors(corsOptions), async (req, res) => {
     }
     else {
       res.status(200).send('');
-      fetch(req.query.response_url,
-        {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/json',
-        },
-        body: JSON.stringify({
-          "response_type": "in_channel",
-          "text": `*enjoy your 100% valid haiku, ${req.query.user_name}* 🤖 \n   _${req.query.text}_`,
-          "type": "mrkdwn",
-        }).replace('\n','\\n')
-        })
         fetch(req.query.response_url,
           {
           method: 'POST',
@@ -116,6 +104,18 @@ app.get('/api', cors(corsOptions), async (req, res) => {
             "text": `${haiku}`,
           })
           })
+          fetch(req.query.response_url,
+            {
+            method: 'POST',
+            headers: {
+              'content-type': 'application/json',
+            },
+            body: JSON.stringify({
+              "response_type": "in_channel",
+              "text": `*enjoy your 100% valid haiku, ${req.query.user_name}* 🤖 \n   _${req.query.text}_`,
+              "type": "mrkdwn",
+            }).replace('\n','\\n')
+            })
     }
   }
 })
