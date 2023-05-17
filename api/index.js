ca8176c9c1be3e418a096d81f3d76057d2db3de4
zai-i <zainab@wonderbly.com>
2023-05-17 21:57:17 +0100

    Test

--- api/index.js
+++ api/index.js
@@ -93,7 +93,18 @@ app.get('/api', cors(corsOptions), async (req, res) => {
     }
     else {
       res.status(200).send('');
-        try {
+      fetch(req.query.response_url,
+        {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          "response_type": "in_channel",
+          "text": `*enjoy your valid haiku, ${req.query.user_name}* 🤖 \n   _${req.query.text}_`,
+          "type": "mrkdwn",
+        }).replace('\n','\\n')
+        })
         fetch(req.query.response_url,
           {
           method: 'POST',
@@ -102,24 +113,9 @@ app.get('/api', cors(corsOptions), async (req, res) => {
           },
           body: JSON.stringify({
             "response_type": "in_channel",
-            "text": `*enjoy your valid haiku, ${req.query.user_name}* 🤖 \n   _${req.query.text}_`,
-            "type": "mrkdwn",
-          }).replace('\n','\\n')
+            "text": `${haiku}`,
           })
-          fetch(req.query.response_url,
-            {
-            method: 'POST',
-            headers: {
-              'content-type': 'application/json',
-            },
-            body: JSON.stringify({
-              "response_type": "in_channel",
-              "text": `${haiku}`,
-            })
-            })
-        } catch (error) {
-          console.error(error);
-        }
+          })
     }
   }
 })
