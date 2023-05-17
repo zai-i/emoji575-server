45af934e7e126f6063936a9e9dce478b88863f0d
zai-i <zainab@wonderbly.com>
2023-05-17 16:43:36 +0100

    Empty status

--- api/index.js
+++ api/index.js
@@ -89,11 +89,7 @@ app.get('/api', cors(corsOptions), async (req, res) => {
   try {
     const response = await retryRequest(process.env.RAPID_API_URL, options)
     const haiku = smarten(response);
-    res.status(200).send(
-      {
-        "response_type": "ephemeral",
-        "text": ""
-    })
+    res.status(200).send('')
     fetch(req.query.response_url,
       {
       method: 'POST',
