adc648125dc0d65aa813997706c61210899768db
zai-i <zainab@wonderbly.com>
2023-05-17 18:59:19 +0100

    Test

--- api/index.js
+++ api/index.js
@@ -93,7 +93,7 @@ app.get('/api', cors(corsOptions), async (req, res) => {
       res.send(haiku)
     }
     else {
-      res.status(200).send({"response_type": "in_channel"});
+      res.status(200).send({"response_type": "in_channel", text: ""});
       fetch(req.query.response_url,
         {
         method: 'POST',
