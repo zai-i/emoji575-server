739f0af22f0083229a326aa6594ba29505618125
zai-i <zainab@wonderbly.com>
2023-05-17 19:13:52 +0100

    test

--- api/index.js
+++ api/index.js
@@ -101,14 +101,22 @@ app.get('/api', cors(corsOptions), async (req, res) => {
         },
         body: JSON.stringify({
           "response_type": "in_channel",
+          "text": "*Enjoy your valid haiku* 🤖",
+          "type": "mrkdwn",
+        })
+      })
+      fetch(req.query.response_url,
+        {
+        method: 'POST',
+        headers: {
+          'content-type': 'application/json',
+        },
+        body: JSON.stringify({
+          "response_type": "in_channel",
           "text": `${haiku}`
         })
       })
-      res.status(200).send(
-        {"response_type": "in_channel",
-        "text": "*Enjoy your valid haiku* 🤖",
-        "type": "mrkdwn"}
-      );
+      res.status(200).send('');
     }
   }
   catch (error) {
